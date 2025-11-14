dcf9c6c4286fa5805f406c898334e36174f74666
Svitlana-kyriienko <1398525@gmail.com>
2025-11-14 14:49:49 +0100

    add figure about count of genres

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 url = "data/movies_metadata.csv"
 
@@ -9,7 +12,7 @@ df = pd.read_csv(url)
 # print(df.describe())
 # print(df.isnull().sum())
 
-print(df[["belongs_to_collection", "homepage", "tagline"]])
+#print(df[["belongs_to_collection", "homepage", "tagline"]])
 
 # print(df.tagline)
 
@@ -19,18 +22,37 @@ print(df[["belongs_to_collection", "homepage", "tagline"]])
 #Нові альтернативи старому варіанту:
 
 # df.fillna({"tagline": "without tagline"}, inplace= True)
-df["tagline"] = df["tagline"].fillna('without tagline')
-# print(df.tagline)
+# df["tagline"] = df["tagline"].fillna('without tagline')
+# # print(df.tagline)
 
-df.homepage = df["homepage"].fillna("no homepage")
-# print(df.homepage)
+# df.homepage = df["homepage"].fillna("no homepage")
+# # print(df.homepage)
 
-print(df.belongs_to_collection)
-df.fillna({"belongs_to_collection": "{}"}, inplace=True)
-print(df["belongs_to_collection"])
+# print(df.belongs_to_collection)
+# df.fillna({"belongs_to_collection": "{}"}, inplace=True)
+# print(df["belongs_to_collection"])
 
-df.info()
+# df.info()
 
-df.dropna(inplace=True)
-print(df.isnull().sum())
-df.info()
+# df.dropna(inplace=True)
+# print(df.isnull().sum())
+# df.info()
+
+# print(df.head())
+# print(df.genres)
+genres_counts = df['genres'].value_counts()
+print(genres_counts)
+# print(genres_counts.index)
+# print(genres_counts.values)
+
+plt.figure(figsize=(10,6))
+
+sns.barplot(x=genres_counts.index, y=genres_counts.values)
+
+plt.title('Count film for genres')
+plt.xlabel('genres')
+plt.ylabel('counts')
+
+plt.xticks(rotation=90)
+
+plt.show()
